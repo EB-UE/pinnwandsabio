25e067d8842df706c5a9456377a9a174badab04a
Janosch Schwalm <janosch.schwalm@eb.de>
2022-02-11 22:13:59 +0100

    remove dead code

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
   <v-app>
     <v-main>
-      <!-- <HelloWorld /> -->
       <Pinnwand></Pinnwand>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Pinnwand from "./components/pinnwand/Pinnwand.vue";
 
 export default {
   name: "App",
 
   components: {
-    HelloWorld,
     Pinnwand,
   },
 
