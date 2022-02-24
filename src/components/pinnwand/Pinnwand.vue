f3b9cfad79ad8612d636dcaa1ff670ca1d968ef4
Janosch Schwalm <janosch.schwalm@eb.de>
2022-02-24 14:54:59 +0100

    remove querstion marks

--- src/components/pinnwand/Pinnwand.vue
+++ src/components/pinnwand/Pinnwand.vue
@@ -23,21 +23,21 @@
       <v-col cols="4" align="center" >
         <v-icon class="super-large-icon mb-2" color="grey">mdi-account-search</v-icon>
         <button-with-link
-          buttonText="die Suche benutzen?"
+          buttonText="die Suche benutzen"
           link="https://eb.sabio.de/web/#!/fullscreen/_id/1644246169/text/_id/520f21a91e074501b866b43f42c8d57d"
         ></button-with-link>
       </v-col>
       <v-col cols="4" align="center" >
         <v-icon class="super-large-icon mb-2" color="grey">mdi-eye</v-icon>
         <button-with-link
-          buttonText="die Ansichten steuern?"
+          buttonText="die Ansichten steuern"
           link="https://eb.sabio.de/web/#!/fullscreen/_id/1644246199/text/_id/0607fe0c8034467dafa3e1b8af1875ae"
         ></button-with-link>
       </v-col>
       <v-col cols="4" align="center" >
         <v-icon class="super-large-icon mb-2" color="grey">mdi-lightbulb-on</v-icon>
         <button-with-link
-          buttonText="einen Vorschlag hinzufügen?"
+          buttonText="einen Vorschlag hinzufügen"
           link="https://eb.sabio.de/web/#!/fullscreen/_id/1644246223/text/_id/d10b2c41ac7e48ea9a6a796dd79895fd"
         ></button-with-link>
       </v-col>
